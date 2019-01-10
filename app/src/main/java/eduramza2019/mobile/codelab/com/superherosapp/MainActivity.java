8057c37831c3e087a3e456f82ecc1a4182f1e4dd
eduramza <eduardo.rmb@outlook.com>
2019-01-10 18:38:44 -0200

    Create Project!

---
+++ app/src/main/java/eduramza2019/mobile/codelab/com/superherosapp/MainActivity.java
@@ -0,0 +1,13 @@
+package eduramza2019.mobile.codelab.com.superherosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
